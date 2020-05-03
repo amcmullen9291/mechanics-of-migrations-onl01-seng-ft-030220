5df33064836afb7e6e39633e5ba3ae504ba61de6
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-03 07:26:06 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,11 +1,2 @@
-
-class CreateArtists < ActiveRecord::Migration[6.0.0]
-  def change
-    create_table :artists do |t|
-      t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
-    end
-  end
+class Artist < ActiveRecord::Base
 end
